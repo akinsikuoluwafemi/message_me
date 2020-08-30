f2d5a582b2ac5388a271f43b401de03d5d395949
Akinsiku Femi <akinsiku13@gmail.com>
2020-08-30 08:50:21 +0100

    Limit the message fetching to 15 most recent ones after implementing web sockets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @messages = Message.all
+    # @messages = Message.all
+    @messages = Message.last(15)
+
     @message = Message.new
   end
 end
